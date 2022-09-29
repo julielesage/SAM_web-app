5d1ac2b887bb0ad916b03604f79792fbe1c077cc
julielesage <lesage.julie@gmail.com>
2022-09-29 21:41:48 +0200

    add privacy-policy

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import {Link}  from "react-router-dom";
 
 const Home = () => {
   return (
 <>
 <p>home here </p>
+<Link to="/privacy-policy">Privacy Policy</Link>
 </>
 );
 };
